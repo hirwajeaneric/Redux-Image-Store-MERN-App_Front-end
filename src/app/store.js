42528bbe31b4884c604b4658b64719370d0b49ff
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-05 08:46:12 +0200

    Done configuring the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import imageReducer from '../features/images/imageSlice';
+import filterReducer from '../features/filter/filterSlice';
+import responseReducer from '../features/response/responseSlice';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     image: imageReducer,
+    filter: filterReducer,
+    response: responseReducer,
   },
 });
