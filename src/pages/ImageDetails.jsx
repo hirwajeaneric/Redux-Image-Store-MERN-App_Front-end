a67ceefa0748e286999f1d403abb8e76a817fcea
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-07 18:44:07 +0200

    Code refactor

--- src/pages/ImageDetails.jsx
+++ src/pages/ImageDetails.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react'
 import { BodyContainer, ImageDetailsContainer } from '../components/styledComponents/BodyStyles';
 import { Button } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getImageDetails } from '../redux/features/images/imageSlice';
 import { links } from '../utils/Apis';
+// Down
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+// Up
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export default function ImageDetails() {
   const params = useParams();
@@ -39,8 +44,8 @@ export default function ImageDetails() {
                 <div className='buttons'>
                   <Button size='small' color='primary' variant='contained' type='button' onClick={() => navigate(`/edit/${selectedImage._id}`)}>Go to Edit</Button>
                   <div>
-                    <Button size='small' color='secondary' variant='contained' type='submit'><ArrowBackIcon /> Previous</Button>
-                    <Button size='small' color='secondary' variant='contained' type='submit'>Next <ArrowForwardIcon /></Button>
+                    <Button size='small' color='secondary' variant='contained' type='submit'><ThumbUpOffAltIcon />&nbsp;Like</Button>
+                    <Button size='small' color='secondary' variant='contained' type='submit'><FavoriteBorderIcon />&nbsp;Add to Favorites</Button>
                   </div>
                 </div>
               </div>
--- src/pages/UpdateImage.jsx
+++ src/pages/UpdateImage.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react'
 import { BodyContainer, ImageDetailsContainer } from '../components/styledComponents/BodyStyles';
 import { Button, TextField } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getImageDetails } from '../redux/features/images/imageSlice';
 import { links } from '../utils/Apis';
+// Down
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+// Up
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export default function UpdateImage() {
   const params = useParams();
@@ -57,11 +62,12 @@ export default function UpdateImage() {
               </div>
               <form className='image-descriptions'>
                 <TextField id="name" label="Image name" size='small' name='label' onChange={handleLabel} variant="outlined" />
+                <p>Likes:<br/><strong>{selectedImage.likes}</strong></p>
                 <div className='buttons'>
                   <Button size='small' color='primary' variant='contained' type='submit'>CONFIRM EDIT</Button>
                   <div>
-                    <Button size='small' color='secondary' variant='contained' type='submit'><ArrowBackIcon /> Previous</Button>
-                    <Button size='small' color='secondary' variant='contained' type='submit'>Next <ArrowForwardIcon /></Button>
+                    <Button size='small' color='secondary' variant='contained' type='submit'><ThumbUpOffAltIcon />&nbsp;Like</Button>
+                    <Button size='small' color='secondary' variant='contained' type='submit'><FavoriteBorderIcon />&nbsp;Add to Favorites</Button>
                   </div>
                 </div>
               </form>
